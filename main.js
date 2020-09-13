d0b362c628155be0fe53081f324bd2e0cd2d885e
Mykhailo Chui <mihach0493@gmail>
2020-09-13 14:42:19 +0200

    js13 firstone

---
+++ main.js
@@ -0,0 +1,20 @@
+let myHeading = document.querySelector('button');
+myHeading.textContent = 'dodaj';
+
+myHeading.onclick = function () {
+    alert('Lorem ipsum dolor sit')
+}
+console.log(myHeading);
+
+
+
+let text = document.querySelector('button');
+text.textContent = 'usun';
+
+text.onclick = function () {
+    alert()
+}
+console.log(text);
+
+
+
