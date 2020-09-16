7e32712eb3bde1a3c5f16fd7015d6cbba2bd129a
Mykhailo Chui <mihach0493@gmail>
2020-09-16 18:24:08 +0200

    zmiana1

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 let myHeading = document.querySelector('button');
 
-let buttonAdd = document.querySelector('#dodaj');
-let buttonRemove = document.querySelector('#usun');
+let buttonAdd = document.querySelector('.dodaj');
+let buttonRemove = document.querySelector('.usun');
+let pText = document.querySelector('.tekst');
 
 buttonAdd.onclick = function () {
 
-   
+    pText.textContent = "test text";
+    
 }
 
-
+buttonRemove.onclick = function () {
+    pText.textContent = "";
+}
 
